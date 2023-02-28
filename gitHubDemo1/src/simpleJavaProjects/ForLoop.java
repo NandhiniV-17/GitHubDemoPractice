b9fc54af7547a6bd387807f75ea92de0c31159d7
NandhiniV-17 <117360180+NandhiniV-17@users.noreply.github.com>
2023-02-28 10:27:11 -0500

    For Practice

---
+++ gitHubDemo1/src/simpleJavaProjects/ForLoop.java
@@ -0,0 +1,13 @@
+package simpleJavaProjects;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int a=0;
+		int b=15;
+		System.out.println ("The even numbers between "+a+" and "+b+" are as follows ");
+		for (int i=a;i<=b;i+=2)
+			System.out.println (+i);
+		
+}
+}
